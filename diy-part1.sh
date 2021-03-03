da13b9fece6e58593550340247cb9e4b462d2332
nanchuci <71806013+nanchuci@users.noreply.github.com>
2021-03-04 01:00:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,13 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git lean https://github.com/coolsnowwolf/packages' feeds.conf.default
 
 # Define My Package
 #git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
 #git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
